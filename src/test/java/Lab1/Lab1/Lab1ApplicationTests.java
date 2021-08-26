c67b7d4a1486dc94077804b271194902f192a036
Jairo Pulido <jairo.pulido@mail.escuelaing.edu.co>
2021-08-25 21:36:12 -0500

    Revisiones

--- src/test/java/Lab1/Lab1/Lab1ApplicationTests.java
+++ src/test/java/Lab1/Lab1/Lab1ApplicationTests.java
@@ -1,13 +1,2 @@
-package Lab1.Lab1;
+//package Lab1.Lab1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Lab1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
